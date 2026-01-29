7507adec88d805b1f6d3051a1c8f222eec5835eb
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-01-29 20:20:23 +0100

    prpr.c: 9th version provided by Roberto
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- prpr.c
+++ prpr.c
@@ -5,6 +5,8 @@
 
 #define MAX_BLOCK_SIZE 512
 
+#define ABS(a) { (a < 0) ? -a : a }
+
 static inline void reverse_buffer(unsigned char *buf, size_t size) {
     size_t i, j;
     for (i = 0, j = size - 1; i < j; i++, j--) {
@@ -27,13 +29,11 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    size_t w_size = (size_t)(w_arg < 0 ? -w_arg : w_arg);
-    size_t o_abs = (size_t)(o_arg < 0 ? -o_arg : o_arg);
+    size_t w_size = (size_t)ABS(w_arg);
+    size_t o_abs = (size_t)ABS(o_arg);
 
-    // 1. STOP Condition: If absolute values are equal and w was negative
-    if (w_arg < 0 && w_size == o_abs) {
-        return 0; 
-    }
+    // 1. symetric STOP Condition: If absolute values are equal
+    if (w_size == o_abs) return 0;
 
     if (w_size == 0 || w_size > MAX_BLOCK_SIZE) {
         fprintf(stderr, "Error: Window size invalid.\n");
@@ -48,34 +48,41 @@ int main(int argc, char *argv[]) {
     unsigned char buffer[MAX_BLOCK_SIZE];
 
     while (1) {
-        size_t bytes_read = 0;
+        size_t bytes_read = 0, nw = 0, nr = 0;
         while (bytes_read < w_size) {
-            ssize_t n = read(STDIN_FILENO, buffer + bytes_read, w_size - bytes_read);
-            if (n == 0) return 0;
-            if (n < 0) {
+            nr = read(STDIN_FILENO, buffer + bytes_read, w_size - bytes_read);
+            if (nr == 0) return 0;
+            if (nr < 0) {
                 if (errno == EINTR) continue;
                 perror("read");
                 exit(EXIT_FAILURE);
             }
-            bytes_read += n;
+            bytes_read += nr;
         }
 
         // 2. Processing Logic
         if (o_abs == w_size) {
             // Reversal logic (triggered by equal abs values, but not the STOP case)
             reverse_buffer(buffer, w_size);
-            write(STDOUT_FILENO, buffer, w_size);
+            nw = write(STDOUT_FILENO, buffer, w_size);
         } 
         else if (w_arg < 0) {
             // Centered Mode: Logic (w-o)/2
             // Note: This implementation assumes we print o_abs chars starting at 'start'
-            size_t start = (w_size - o_abs) / 2;
-            write(STDOUT_FILENO, buffer + start, o_abs);
+            size_t n = (w_size - o_abs) / 2;
+            nw = write(STDOUT_FILENO, buffer, n);
+            n += o_abs; //RAF jump haed
+            if(w_size > n)
+              nw = write(STDOUT_FILENO, buffer + n, w_size - n);
         } 
         else {
             // Standard Slice Mode (Head/Tail)
             unsigned char *ptr = (o_arg >= 0) ? buffer : (buffer + (w_size - o_abs));
-            write(STDOUT_FILENO, ptr, o_abs);
+            nw = write(STDOUT_FILENO, ptr, o_abs);
+        }
+        if(nw < 0) {
+          perror("write");
+          exit(EXIT_FAILURE);
         }
     }
     return 0;
