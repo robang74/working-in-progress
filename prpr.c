751e9f3eb12cdaa2b816b06fbd8174a6f6b8b818
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-01-29 19:25:01 +0100

    prpr.c: 4th version provided by Roberto
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- prpr.c
+++ prpr.c
@@ -2,18 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-#include <string.h>
-
-#define MAX_PAGE_SIZE 4096
 
 int main(int argc, char *argv[]) {
+    size_t MAX_PAGE_SIZE = getpagesize();
     int opt;
-    long o_val = 0;   // Signed offset/slice value
+    long o_size = 0; // Signed offset/slice value
     size_t w_size = 0; // Window (stride) size
 
     while ((opt = getopt(argc, argv, "o:w:")) != -1) {
         switch (opt) {
-            case 'o': o_val = strtol(optarg, NULL, 10); break;
+            case 'o': o_size = strtol(optarg, NULL, 10); break;
             case 'w': w_size = strtoul(optarg, NULL, 10); break;
             default: exit(EXIT_FAILURE);
         }
@@ -21,13 +19,13 @@ int main(int argc, char *argv[]) {
 
     // Constraints check
     if (w_size == 0 || w_size > MAX_PAGE_SIZE) {
-        fprintf(stderr, "Error: Window (-w) must be between 1 and %d\n", MAX_PAGE_SIZE);
+        fprintf(stderr, "Error: Window (-w) must be between 1 and %ld\n", MAX_PAGE_SIZE);
         exit(EXIT_FAILURE);
     }
 
-    size_t abs_o = (o_val < 0) ? -o_val : o_val;
-    if (abs_o > w_size) {
-        fprintf(stderr, "Error: Absolute offset |%ld| cannot exceed window %zu\n", o_val, w_size);
+    size_t abs_o_size = (o_size < 0) ? -o_size : o_size;
+    if (abs_o_size > w_size) {
+        fprintf(stderr, "Error: Absolute offset |%ld| cannot exceed window %zu\n", o_size, w_size);
         exit(EXIT_FAILURE);
     }
 
@@ -35,17 +33,17 @@ int main(int argc, char *argv[]) {
     unsigned char *output_ptr;
     size_t bytes_to_write;
 
-    // Logic: 
-    // If o is 3: print first 3 (buffer, len 3)
-    // If o is -1: print last 1 (buffer + (4-1), len 1)
-    if (o_val >= 0) {
+    // Logic, considering -w 4: 
+    // -o  3: print first 3 (buffer, len 3)
+    // -o -1: print last 1 (buffer + (4-1), len 1)
+    if (o_size >= 0) {
         output_ptr = buffer;
-        bytes_to_write = (size_t)o_val;
+        bytes_to_write = (size_t)o_size;
     } else {
-        output_ptr = buffer + (w_size - abs_o);
-        bytes_to_write = abs_o;
+        output_ptr = buffer + (w_size - abs_o_size);
+        bytes_to_write = abs_o_size;
     }
-
+    
     while (1) {
         size_t bytes_read = 0;
 
