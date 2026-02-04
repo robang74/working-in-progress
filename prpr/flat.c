b964d3d97740dc3358d75b06eec5cbddbdbb8570
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-02-04 20:52:59 +0100

    prpr/flat.c: refactorying, p.1
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- prpr/flat.c
+++ prpr/flat.c
@@ -16,10 +16,29 @@
 
 #define MAX_READ_SIZE 4096
 
+unsigned printstats(const char *str, size_t nread, unsigned nsymb, size_t *counts) {
+  size_t i;
+  double x, px, k = 0, s = 0, entropy = 0, n = 0, lg2s = log2(nsymb);
+
+  for (i = 0; i < 256; i++) {
+      double ex = ((double)nread) / nsymb, epx = 1.0 / nsymb;
+      x = ((double)counts[i] - ex);
+      s += x * x  / ex;
+      px = ((double)counts[i]) / nread;
+      x = px - epx;
+      k += x * x;
+      if(counts[i]) n++; else continue;
+      entropy -= px * log2(px);
+  }
+  printf("%s: %4ld, Eñ: %.6lf / %.2f = %.6lf, X²: %5.3lf, k²: %3.5lf\n",
+      str, nread, entropy, lg2s, entropy/lg2s, s, k * nsymb);
+
+  return n;
+}
+
 int main(int argc, char *argv[]) {
-    size_t bytes_read = 0, nr = 0, i;  
+    size_t bytes_read = 0, nr = 0, i, counts[256];  
     unsigned char buffer[MAX_READ_SIZE];
-    size_t counts[256];
     
     for (i = 0; i < 256; i++)
         counts[i] = 0;
@@ -37,41 +56,29 @@ int main(int argc, char *argv[]) {
         bytes_read += nr;
     }
 
-  double x, k, s, px, entropy, n = 0;
-
-  for (entropy = 0, s = 0, k = 0, i = 0; i < 256; i++) {
-      if(counts[i]) n++;
-      double ex = ((double)bytes_read) / 256.0, epx = 1.0 / 256.0;
-      x = ((double)counts[i] - ex);
-      s += x * x  / ex;
-      px = ((double)counts[i]) / bytes_read;
-      x = px - epx;
-      k += x * x; // TODO
-      if(!counts[i]) continue;
-      entropy -= px * log2(px);
-
-  }
-  printf("bytes: %4ld, Eñ: %.6lf / 8.00 = %.6lf, X²: %5.3lf, k²: %2.6lf\n",
-      bytes_read, entropy, entropy/8, s, k * 256);
+  unsigned n = printstats("bytes", bytes_read, 256, counts);
 
   double lg2s = log2(n);
   unsigned nbits = ceil(lg2s), nmax = 1 << nbits;
 
   if (nbits < 8) {
+      (void)printstats("encdg", nmax, nmax, counts);
+/*
       for (entropy = 0, s = 0, k = 0, i = 0; i < 256; i++) {
           double ex = ((double)bytes_read) / nmax, epx = 1.0 / nmax;
           x = ((double)counts[i] - ex);
           s += x * x  / ex;
           px = ((double)counts[i]) / nmax;
           x = px - epx;
-          k += x * x; // TODO
+          k += x * x;
           if(!counts[i]) continue;
           entropy -= px * log2(px);
       }
       printf("encdg: %4u, Eñ: %.6lf / %u.00 = %.6lf, X²: %5.3lf, k²: %2.6lf\n",
           (unsigned) nmax, entropy, nbits, entropy/nbits, s, k * n);
+*/
   }
-
+/*
   if(n < nmax) {
       for (entropy = 0, s = 0, k = 0, i = 0; i < 256; i++) {
           double ex = ((double)bytes_read) / n, epx = 1.0 / n;
@@ -87,11 +94,6 @@ int main(int argc, char *argv[]) {
       printf("symbl: %4u, Eñ: %.6lf / %.2f = %.6lf, X²: %5.3lf, k²: %2.6lf\n",
           (unsigned) n, entropy, lg2s, entropy/lg2s, s, k * n);
   }
-
-/* 
-   for (i = 0; i < 256; i++) {
-      if(counts[i])
-        printf("%3ld: %6ld\n", i, counts[i]);
-   }
 */
+    return 0;
 }
