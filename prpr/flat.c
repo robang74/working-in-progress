cf2b5e7f281877dfb41befa0c60f221a0df05db4
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-02-04 19:12:31 +0100

    prpr/flat.c: frequency k² and entropy over encoding bits
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- prpr/flat.c
+++ prpr/flat.c
@@ -37,32 +37,33 @@ int main(int argc, char *argv[]) {
         bytes_read += nr;
     }
 
-  unsigned int n = 0;
-  double x, k, s, entropy;
+  double x, k, s, px, entropy, n = 0;
 
   for (entropy = 0, s = 0, k = 0, i = 0; i < 256; i++) {
       if(counts[i]) n++;
       double ex = ((double)bytes_read) / 256.0;
       x = ((double)counts[i] - ex);
       s += x * x  / ex;
+      px = ((double)counts[i]) / bytes_read;
+      x = px - (1.0 / 256.0);
+      k += x * x; // TODO
       if(!counts[i]) continue;
-      x = ((double)counts[i]) / bytes_read;
-      entropy -= x * log2(x);
-      x -= ex;
-      x *= x;
-      k += x; // TODO
+      entropy -= px * log2(px);
+
   }
-  printf("bytes: %4ld, E: %12lf, X²: %12lf\n", bytes_read, entropy, s);
+  printf("bytes: %4ld, Eñ: %12.6lf / 8, X²: %12.6lf, k²: %12.6lf\n", bytes_read, entropy, s, k * 256);
 
   for (entropy = 0, s = 0, k = 0, i = 0; i < 256; i++) {
       if(!counts[i]) continue;
       double ex = ((double)bytes_read) / n;
       x = ((double)counts[i] - ex);
       s += x * x  / ex;
-      x = ((double)counts[i]) / n;
-      entropy -= x * log2(x);
+      px = ((double)counts[i]) / n;
+      x = px - (1.0 / n);
+      k += x * x; // TODO
+      entropy -= px * log2(px);
   }
-  printf("symbl: %4u, E: %12lf, X²: %12lf\n", n, entropy, s);
+  printf("symbl: %4u, Eñ: %12.6lf / %u, X²: %12.6lf, k²: %12.6lf\n", (unsigned) n, entropy, (unsigned) ceil(log2(n)), s, k * n);
 /* 
    for (i = 0; i < 256; i++) {
       if(counts[i])
