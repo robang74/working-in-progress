d8b112e9ea916242ee33ce60ec1da3a353174ab5
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-02-12 13:06:31 +0100

    prpr/uchaos.sh: release v0.1.4, for testing in github docker VM
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- prpr/uchaos.sh
+++ prpr/uchaos.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+nfle=${1:-test.txt}
+echo "Appending: $nfle"
 time for i in $(seq 0 7); do
-    printf "\n### using -s $i\n";
+    printf "\n### using -s $i -d 7\n";
     cat uchaos.c | ./uchaos -T 10000 -s $i -d 7 | ent;
-done >> test.txt 2>&1
+done >> $nfle 2>&1
+time for i in $(seq 0 7); do
+    printf "\n### using -s $i -d 15\n";
+    cat uchaos.c | ./uchaos -T 10000 -s $i -d 15 | ent;
+done >> $nfle 2>&1
