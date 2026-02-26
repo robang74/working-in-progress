2cea393d03c8f4cc6617e0a03e6e9c1a80a72b7e
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-02-26 12:18:20 +0100

    prpr/uctest.sh: preliminary test added
    CAN WE IMPROVE RANDOMNESS? NOT REALLY!
    
    - lnkd.in/d25rMexE
    
    - linkedin.com/posts/
       robertofoglietta_can-we-improve-randomness-not-really-this-activity-7432739785695383552-8ImI
    
    - facebook.com/roberto.a.foglietta/posts/10162995073383736
    
    cat $(ls -1 test-*.dat | sort) | ~/Downloads/PractRand_0.96/PractRand/RNG_test stdin64
    RNG_test using PractRand version 0.96
    RNG = RNG_stdin64, seed = unknown
    test set = core, folding = standard (64 bit)
    
    rng=RNG_stdin64, seed=unknown
    length= 256 megabytes (2^28 bytes), time= 2.6 seconds
      Test Name                         Raw       Processed     Evaluation
      BCFN(2+0,13-2U)                   R>+99999  p = 0           FAIL !!!!!!!!
      BCFN(2+1,13-2U)                   R>+99999  p = 0           FAIL !!!!!!!!
      BCFN(2+2,13-3U)                   R>+99999  p = 0           FAIL !!!!!!!!
      BCFN(2+3,13-3U)                   R>+99999  p = 0           FAIL !!!!!!!!
      BCFN(2+4,13-3U)                   R>+99999  p = 0           FAIL !!!!!!!!
      BCFN(2+5,13-4U)                   R>+99999  p = 0           FAIL !!!!!!!!
      BCFN(2+6,13-5U)                   R>+99999  p = 0           FAIL !!!!!!!!
      BCFN(2+7,13-5U)                   R>+99999  p = 0           FAIL !!!!!!!!
      BCFN(2+8,13-6U)                   R>+99999  p = 0           FAIL !!!!!!!!
      BCFN(2+9,13-6U)                   R>+99999  p = 0           FAIL !!!!!!!!
      BCFN(2+10,13-7U)                  R=+69095  p = 0           FAIL !!!!!!!!
      BCFN(2+11,13-8U)                  R=+40957  p = 0           FAIL !!!!!!!!
      BCFN(2+12,13-8U)                  R=+20441  p =  6e-5292    FAIL !!!!!!!!
      BCFN(2+13,13-9U)                  R=+11734  p =  4e-2643    FAIL !!!!!!!!
      BCFN(2+14,13-9U)                  R= +5853  p =  7e-1319    FAIL !!!!!!!!
      DC6-9x1Bytes-1                    R=+40149  p = 0           FAIL !!!!!!!!
      Gap-16:A                          R=+58919  p = 0           FAIL !!!!!!!!
      Gap-16:B                          R>+99999  p = 0           FAIL !!!!!!!!
      Gap-16:C                          R= +1558  p~=1-6.7e-470   FAIL !!!!!!!
      FPF/16:(1,14-0)                   R=+68555  p = 0           FAIL !!!!!!!!
      FPF/16:(2,14-0)                   R=+34276  p = 0           FAIL !!!!!!!!
      FPF/16:(3,14-0)                   R=+17114  p = 0           FAIL !!!!!!!!
      FPF/16:(4,14-1)                   R=+12128  p = 0           FAIL !!!!!!!!
      FPF/16:(5,14-2)                   R= +8592  p =  3e-7514    FAIL !!!!!!!!
      FPF/16:(6,14-2)                   R= +4303  p =  3e-3763    FAIL !!!!!!!!
      FPF/16:(7,14-3)                   R= +3034  p =  2e-2659    FAIL !!!!!!!!
      FPF/16:(8,14-4)                   R= +2146  p =  1e-1753    FAIL !!!!!!!!
      FPF/16:(9,14-5)                   R= +1520  p =  3e-1260    FAIL !!!!!!!!
      FPF/16:(10,14-5)                  R=+755.6  p =  2.6e-626   FAIL !!!!!!!
      FPF/16:(11,14-6)                  R=+542.2  p =  5.6e-415   FAIL !!!!!!!
      FPF/16:(12,14-7)                  R=+395.2  p =  2.5e-314   FAIL !!!!!!
      FPF/16:(13,14-8)                  R=+276.1  p =  1.0e-198   FAIL !!!!!!
      FPF/16:(14,14-8)                  R=+142.8  p =  8.9e-103   FAIL !!!!!
      FPF/16:all                        R=+59638  p = 0           FAIL !!!!!!!!
      FPF/16:cross                      R=+162.6  p =  3.8e-137   FAIL !!!!!
      BDayS2(4,24)[51]                  R= +21.7  p~=  1.3e-67    FAIL !!!!
      BRank(12):score:1792              R= +1436  p~=  1e-1298    FAIL !!!!!!!!
      ...and 162 test result(s) without anomalies
    
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- prpr/uctest.sh
+++ prpr/uctest.sh
@@ -2,18 +2,20 @@
 #
 # (c) 2026, Roberto A. Foglietta <roberto.foglietta@gmail.com>, MIT license
 #
-# output to test with: cat *.dat | PractRand_0.96/PractRand/RNG_test stdin64
+# preliminary internal check: grep KO test-*.txt
+# output to test with: cat $(ls -1 test-*.dat | sort) | RNG_test stdin64
 #
 
 for i in $(seq 32); do
 
-for t in $(seq 4); do
-  n=$(printf "%04d" $i)
-  echo "test-$t-$n.txt test-$t-$n.dat"
-  ({ time cat dmesg.txt | ./uchaos -i16 -T $((256*1024)) | tee test-$t-$n.dat | ent; } > test-$t-$n.txt 2>&1) &
-  sleep 0.1
-done
+  for t in $(seq 4); do
+    n=$(printf "%04d" $i)
+    echo "test-$t-$n.txt test-$t-$n.dat"
+    ({ time cat dmesg.txt | ./uchaos -i16 -T $((256*1024)) | tee test-$t-$n.dat | ent; } > test-$t-$n.txt 2>&1) &
+    sleep 0.1
+  done
 
-wait
+  wait
+  grep -n KO test-*.txt && break
 
 done
