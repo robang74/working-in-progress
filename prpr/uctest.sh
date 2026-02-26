fd7543b2288122a2dc29ccc1d3492f89410a8790
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-02-26 11:09:03 +0100

    prpr/uctest.sh: shell script to generate 16GB of data, p.2
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- prpr/uctest.sh
+++ prpr/uctest.sh
@@ -1,24 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 #
 # (c) 2026, Roberto A. Foglietta <roberto.foglietta@gmail.com>, MIT license
 #
 # output to test with: cat *.dat | PractRand_0.96/PractRand/RNG_test stdin64
 #
 
-for x in $(seq 32); do
+for i in $(seq 32); do
 
 for t in $(seq 4); do
-
-for i in $(seq 1); do
-
-n=$(printf "%04d" $i)
-
-{ time cat dmesg.txt | ./uchaos -i16 -T$[1024*256] | tee test-$t-$n.dat |  ent; } >> test-$t-$n.txt 2>&1 &
-
+  n=$(printf "%04d" $i)
+  echo "test-$t-$n.txt test-$t-$n.dat"
+  ({ time cat dmesg.txt | ./uchaos -i16 -T $((256*1024)) | tee test-$t-$n.dat | ent; } > test-$t-$n.txt 2>&1) &
+  sleep 0.1
 done
 
 wait
-
-done
 
 done
