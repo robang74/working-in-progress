88f17944b90e7e82a8d9b99e96ccf34cb31d1b2d
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-02-27 10:38:30 +0100

    prpr/uctest.sh: using 'uchaos -d7' to face the 64GB test again, p.2
    cat test-*.dat | ~/Downloads/PractRand_0.96/PractRand/RNG_test stdin64
    RNG_test using PractRand version 0.96
    RNG = RNG_stdin64, seed = unknown
    test set = core, folding = standard (64 bit)
    
    rng=RNG_stdin64, seed=unknown
    length= 64 megabytes (2^26 bytes), time= 2.1 seconds
      no anomalies in 172 test result(s)
    
    rng=RNG_stdin64, seed=unknown
    length= 128 megabytes (2^27 bytes), time= 5.2 seconds
      no anomalies in 185 test result(s)
    
    rng=RNG_stdin64, seed=unknown
    length= 256 megabytes (2^28 bytes), time= 10.4 seconds
      no anomalies in 199 test result(s)
    
    rng=RNG_stdin64, seed=unknown
    length= 512 megabytes (2^29 bytes), time= 19.6 seconds
      no anomalies in 213 test result(s)
    
    rng=RNG_stdin64, seed=unknown
    length= 1 gigabyte (2^30 bytes), time= 37.0 seconds
      no anomalies in 227 test result(s)
    
    rng=RNG_stdin64, seed=unknown
    length= 2 gigabytes (2^31 bytes), time= 71.3 seconds
      no anomalies in 242 test result(s)
    
    rng=RNG_stdin64, seed=unknown
    length= 4 gigabytes (2^32 bytes), time= 138 seconds
      no anomalies in 256 test result(s)
    
    rng=RNG_stdin64, seed=unknown
    length= 8 gigabytes (2^33 bytes), time= 262 seconds
      no anomalies in 270 test result(s)
    
    rng=RNG_stdin64, seed=unknown
    length= 16 gigabytes (2^34 bytes), time= 522 seconds
      no anomalies in 283 test result(s)
    
    rng=RNG_stdin64, seed=unknown
    length= 32 gigabytes (2^35 bytes), time= 1027 seconds
      no anomalies in 296 test result(s)
    
    rng=RNG_stdin64, seed=unknown
    length= 64 gigabytes (2^36 bytes), time= 2007 seconds
      no anomalies in 308 test result(s)
    
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- prpr/uctest.sh
+++ prpr/uctest.sh
@@ -6,16 +6,29 @@
 # output to test with: cat $(ls -1 test-*.dat | sort) | RNG_test stdin64
 #
 
+fn="test"
+ch=$((1024*1024))
+ch="./uchaos -i16 -d 7 -T $ch"
+tf() { time cat dmesg.txt | $ch | tee $1.dat | ent; }
+
 for i in $(seq 32); do
 
   for t in $(seq 4); do
     n=$(printf "%04d" $i)
-    echo "test-$t-$n.txt test-$t-$n.dat"
-    ({ time cat dmesg.txt | ./uchaos -i16 -d 7 -T $((1024*1024)) | tee test-$t-$n.dat | ent; } > test-$t-$n.txt 2>&1) &
+    nm="$fn-$t-$n"
+    echo "$nm.txt $nm.dat"
+    { tf $nm; } > $nm.txt 2>&1 &
     sleep 0.1
   done
 
   wait
-  grep -n KO test-*.txt && break
+  eq=$(du -k $(find . -name $fn-*.dat | sort) | cut -f1 | tr '\n' +)0
+  echo "$(( ( $eq ) >> 10 )) MB"
+  grep -n "KO" $fn-*.txt &&
+    break
 
 done
+
+echo
+echo 'to test with: cat $(ls -1 test-*.dat | sort) | RNG_test stdin64'
+echo
